e6c56ae8ea111cb769aba2ad498d1939331cd002
Christa Lococo <montecitocandleco@gmail.com>
2023-10-27 12:43:25 -0600

    homework

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,15 +1,44 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
+const Category = require('./Category'); // Import the Category model
+const Tag = require('./Tag'); // Import the Tag model
 
-// Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Category,
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
@@ -19,5 +48,19 @@ Product.init(
     modelName: 'product',
   }
 );
+
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+  foreignKey: 'product_id',
+});
+
+Product.belongsToMany(Tag, {
+  through: 'product_tag',
+  foreignKey: 'product_id',
+});
 
 module.exports = Product;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection');
+const routes = require('./routes'); // Import your routes
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,9 +8,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+app.use(routes); // Use your routes
+
+// Sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
 });
